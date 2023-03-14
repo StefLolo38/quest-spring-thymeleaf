654767503f210d14eba1c11c5451b101878673be
BAILLOT STEPHANE <[stefbbst@gmail.com](stefbbst@gmail.com)>
2023-03-14 09:06:07 +0100

    first commit

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -3,14 +3,19 @@ package com.example.thymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor")
+    @ResponseBody
     public String doctor(@RequestParam(required=false, defaultValue="0") int number,
-                         @RequestParam(required=false, defaultValue="John Smith") String name) {
+                         @RequestParam(required=false, defaultValue="John Smith") String name)
+    {
 
-        return "doctor";
+        return "doctor" + number + name;
     }
 }
